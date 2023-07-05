1d2632236c9bfc09f58015d6037040ed47273c59
ThapaVinay <thechampthapa@gmail.com>
2023-07-05 22:12:43 +0530

    api call and basic website done

--- index.js
+++ index.js
@@ -1,7 +1,73 @@
 
+
 function App() {
-    return(
-        <h2 className="d-flex justify-content-center">Image Background remover</h2>
+    let imageUrl;
+    const handleUpload = (e) => {
+        const fileInput = document.getElementById('file');
+
+        const image = fileInput.files[0];
+        
+        const form = new FormData();
+        form.append("image_file", image);
+        form.append("size", "auto");
+
+        const apiKey = "PVj6azcCBvSXyaPmS3zooCHE";
+
+        fetch("https://api.remove.bg/v1.0/removebg",{
+            method : "POST",
+            headers : {
+                'X-Api-Key' : apiKey,
+            },
+            body : form
+        })
+        .then(function(response)
+        {
+            return response.blob();
+        })
+        .then(function(blob)
+        {
+            console.log(blob);
+
+            const url = URL.createObjectURL(blob);
+            imageUrl = url;
+            const img = document.createElement('img');
+            img.src = url;
+            document.body.appendChild(img);
+        })
+        .catch();
+
+        console.log("clicked");
+    }   
+
+
+    const downloadFile = () => {
+        var anchor = document.createElement('a');
+        anchor.href = imageUrl;
+        anchor.download = "no-bg-png";
+
+        document.body.appendChild(anchor);
+
+        anchor.click();
+
+        document.body.removeChild();
+
+    }
+
+    return (
+        <>
+            <h2 className="d-flex justify-content-center">Image Background remover</h2>
+            <div className="container">
+                <form >
+                    <div className="form-group">
+                        <label htmlFor="file"> Select a file</label>
+                        <input className="form-control" type="file" id="file" name="file" />
+                    </div>
+                    <button type="button" onClick={handleUpload}>Upload</button>
+                    <button className="btn btn-secondary" onClick={downloadFile}> Download </button>
+                </form>
+            </div>
+        </>
+
     );
 }
 
@@ -11,7 +77,7 @@ const root = document.getElementById("app")
 
 const app = (
     <React.StrictMode>
-        <App/>
+        <App />
     </React.StrictMode>
 )
 
